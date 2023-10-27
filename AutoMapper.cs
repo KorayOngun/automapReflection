2b0930113510435fefdc326a822b102bda708246
koray öngün <koraygg28@gmail.com>
2023-10-27 05:37:35 +0300

    Proje dosyası ekle.

---
+++ AutoMapper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automapReflection
+{
+    public static class Mapper
+    {
+        public static T Map<T>(object obj)
+        {
+            Type type = typeof(T);
+            var item = Activator.CreateInstance(type);
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                item.GetType().GetProperty(property.Name)?.SetValue(item, property.GetValue(obj));
+            }
+            return (T)item;
+        }
+
+        public static T Map<T>(IEnumerable obj)
+        {
+            // obj == new List<User>(){...}
+            // ---> T == IEnumerable<UserResponse> 
+
+            Type listType = typeof(T).GetGenericArguments().First();
+            //Type listType = typeof(UserResponse);
+            
+
+            Type listConstructor = typeof(List<>).MakeGenericType(listType);
+            //Type listConstructor = typeof(List<UserResponse>);
+
+
+            IList items = (IList)Activator.CreateInstance(listConstructor);
+            // IList items = new List<UserResponse>(); 
+
+
+            MethodInfo addedMethod = listConstructor.GetMethod("Add");
+            //MethodInfo addedMethod = typeof(List<UserResponse>).GetMethod("Add");
+
+            
+
+            foreach (var o in obj)
+            {
+                var item = Activator.CreateInstance(listType);
+                //var item = new UserResponse();
+
+                
+                foreach (var prop in o.GetType().GetProperties())
+                {
+                    var value = o.GetType().GetProperty(prop.Name).GetValue(o);
+                    //var value = o.Name, o.LastName
+
+                    listType.GetProperty(prop.Name)?.SetValue(item, value);
+                    //item.name = value, item.LastName = value
+                }
+                
+                addedMethod?.Invoke(items, new object[] { item });
+                // items.Add(item);
+            }
+
+            return (T)items;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using automapReflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //singleObjectMap();
+
+        List<User> users = new List<User>();
+        Console.Write("--> list ");
+        for (int i = 0; i < 10; i++)
+        {
+            var user = new User { id = i, Name = $"name {i}", LastName = $"lastname {i}" };
+            users.Add(user);
+        }
+        Console.WriteLine("<--");
+
+        Console.Write("--> map ");
+        var userResponse = Mapper.Map<IEnumerable<UserResponse>>(users);
+        Console.WriteLine("<--");
+
+        foreach (var item in userResponse)
+        {
+            Console.WriteLine(item.Name);
+            Console.WriteLine(item.LastName);
+        }
+
+    }
+
+    private static void singleObjectMap()
+    {
+        User u = new User
+        {
+            id = 1,
+            Name = "test",
+            LastName = "test lastname",
+        };
+
+
+        var user = Mapper.Map<UserResponse>(u);
+
+        Console.WriteLine(user.Name);
+        Console.WriteLine(user.LastName);
+    }
+}
+public class User
+{
+    public int id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
+public class UserResponse
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+
+}
+
+
